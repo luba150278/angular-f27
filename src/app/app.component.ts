b6ff9668a0928df0c70eadc26e5b47d017b95362
Lyubov Myetolkina <luba150278@gmail.com>
2023-12-14 21:15:46 +0200

    meet1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthModule } from '../auth/auth.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    AuthModule,
+  ],
 })
 export class AppComponent {
-  title = 'angular-f27';
+  title: string = 'angular-f27!!!';
+  desc: string = 'lorem20';
+  getText(): string {
+    return 'Hello, Angular';
+  }
 }
---
+++ src/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  // standalone: true,
+  // imports: [],
+  //templateUrl: './auth.component.html',
+  template: '<div class="container"><p>Auth</p></div>',
+  //styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+}
