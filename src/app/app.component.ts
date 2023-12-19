ef42876dc794e3397b531a910ef7f2411de33ada
Lyubov Myetolkina <luba150278@gmail.com>
2023-12-19 20:27:16 +0200

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthModule } from '../auth/auth.module';
+import { MyTemplateComponent } from './my-template/my-template.component';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +17,31 @@ import { AuthModule } from '../auth/auth.module';
     HeaderComponent,
     FooterComponent,
     AuthModule,
+    MyTemplateComponent
   ],
 })
 export class AppComponent {
+
   title: string = 'angular-f27!!!';
-  desc: string = 'lorem20';
+  desc: string = 'lorem20 LOREM';
+  nowDate: Date = new Date();
+  currencyVal: number = 1000.26689;
+  pi: number = Math.PI;
+  obj: Object = { name: 'Alina', age: 25 };
+  num: number = 6;
+  list: string[] = ['Alina', 'Daria', 'Artur', 'Viktor', 'Stas', 'Yuriy'];
+  name: string = 'Daria';
+  accessToken: string | null = null;
+
+  selectedComponent = MyTemplateComponent;
+
+  id: number = 100;
+  src: string = '/images/bg.jpg'
+
   getText(): string {
     return 'Hello, Angular';
+  }
+  ngOnInit(): void {
+    this.accessToken = localStorage.getItem('accessToken') || null;
   }
 }
---
+++ src/app/my-template/my-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-template.component.html',
+  styleUrl: './my-template.component.css'
+})
+export class MyTemplateComponent {
+
+}
